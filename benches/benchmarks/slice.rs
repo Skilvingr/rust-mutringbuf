ff89af14e6bc7ccafd2a23e6bb41fbc962ad553b
Skilvingr <skilvingr@gmail.com>
2024-05-11 12:29:44 +0200

    add slice clone benchmark

--- benches/benchmarks/slice.rs
+++ benches/benchmarks/slice.rs
@@ -13,6 +13,7 @@ pub fn setup_slices(c: &mut Criterion) {
     c.bench_function("slice_x10", slice_x10);
     c.bench_function("slice_x100", slice_x100);
     c.bench_function("slice_x1000", slice_x1000);
+    c.bench_function("slice_x1000_clone", slice_x1000_clone);
 }
 
 fn slice_x10(b: &mut Bencher) {
@@ -53,6 +54,21 @@ fn slice_x1000(b: &mut Bencher) {
     b.iter(|| {
         prod.push_slice(&data);
         cons.copy_slice(&mut data);
+    });
+
+    black_box(data);
+}
+
+fn slice_x1000_clone(b: &mut Bencher) {
+    let buf = ConcurrentHeapRB::from(vec![0u64; RB_SIZE + 1]);
+    let (mut prod, mut cons) = buf.split();
+
+    prod.push_slice_clone(&[1; 12]);
+
+    let mut data = [1; 1000];
+    b.iter(|| {
+        prod.push_slice_clone(&data);
+        cons.clone_slice(&mut data);
     });
 
     black_box(data);
