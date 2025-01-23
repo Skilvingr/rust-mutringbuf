d990dc371465eeaa43e6a22a9cad445f59baa8c6
Skilvingr <skilvingr@gmail.com>
2025-01-23 15:10:01 +0100

    refactor

--- src/ring_buffer/variants/alloc_ext.rs
+++ src/ring_buffer/variants/alloc_ext.rs
@@ -1,46 +1,51 @@
 #![cfg(feature = "alloc")]
 
-use alloc::vec;
-use alloc::vec::Vec;
-use crate::{ConcurrentMutRingBuf, LocalMutRingBuf, UnsafeSyncCell};
 use crate::HeapStorage;
 #[allow(unused_imports)]
 use crate::ProdIter;
+use crate::{ConcurrentMutRingBuf, LocalMutRingBuf, UnsafeSyncCell};
+use alloc::boxed::Box;
+use alloc::vec;
+use alloc::vec::Vec;
+
+macro_rules! impl_rb {
+    ($t: tt) => {
+        impl<T> From<Vec<T>> for $t<T> {
+            #[doc = concat!("Converts a `Vec<T>` into a [`", stringify!($t), "`].")]
+            /// Note that the length of the buffer will be equal to the length of the vector, and *not*
+            /// to its capacity.
+            fn from(value: Vec<T>) -> Self {
+                Self::_from(HeapStorage::from(value))
+            }
+        }
+        
+        impl<T> $t<T> {
+            #[doc = concat!("Creates a new [`", stringify!($t), "`] with given capacity and zeroed (uninitialised) elements.")]
+            /// # Safety
+            /// The buffer must be then initialised using proper [`ProdIter`] methods (`*_init` ones).
+            pub unsafe fn new_zeroed(capacity: usize) -> Self {
+                Self::_from(
+                    HeapStorage::from(
+                        (0..capacity).map(|_| UnsafeSyncCell::new_zeroed()).collect::<Box<[UnsafeSyncCell<T>]>>()
+                    )
+                )
+            }
+        
+            #[doc = concat!("Creates a new [`", stringify!($t), "`] with given capacity and elements initialised to `default`.")]
+            pub fn default(capacity: usize) -> Self
+                where T: Default + Clone {
+                Self::from(vec![T::default(); capacity])
+            }
+        }
+    };
+}
 
 // Concurrent
 
 /// A heap-allocated ring buffer usable in concurrent environment.
 pub type ConcurrentHeapRB<T> = ConcurrentMutRingBuf<HeapStorage<T>>;
 
-impl<T> From<Vec<T>> for ConcurrentHeapRB<T> {
-    /// Constructs a `ConcurrentHeapRB` using the passed vector.
-    /// Note that the length of the buffer will be equal to the length of the vector, and *not*
-    /// to its capacity.
-    fn from(value: Vec<T>) -> Self {
-        Self::_from(HeapStorage::from(value))
-    }
-}
-
-impl<T> ConcurrentHeapRB<T> {
-    /// Creates a new concurrent heap-allocated buffer with given capacity and zeroed (uninitialised) elements.
-    /// # Safety
-    /// The buffer must be then initialised using proper [`ProdIter`] methods (`*_init` ones).
-    pub unsafe fn new_zeroed(capacity: usize) -> Self {
-        let mut v = Vec::with_capacity(capacity);
-
-        for _ in 0..capacity {
-            v.push(UnsafeSyncCell::new_zeroed());
-        }
-
-        Self::_from(HeapStorage::from(v))
-    }
-
-    /// Creates a new concurrent heap-allocated buffer with given capacity and elements initialised to `default`.
-    pub fn default(capacity: usize) -> Self
-        where T: Default + Clone {
-        Self::from(vec![T::default(); capacity])
-    }
-}
+impl_rb!(ConcurrentHeapRB);
 
 
 // Local
@@ -48,33 +53,4 @@ impl<T> ConcurrentHeapRB<T> {
 /// A heap-allocated ring buffer usable in local environment.
 pub type LocalHeapRB<T> = LocalMutRingBuf<HeapStorage<T>>;
 
-impl<T> From<Vec<T>> for LocalHeapRB<T> {
-    /// Constructs a `LocalHeapRB` using the passed vector.
-    /// Note that the length of the buffer will be equal to the length of teh vector, and *not*
-    /// to its capacity.
-    fn from(mut value: Vec<T>) -> Self {
-        value.shrink_to_fit();
-        Self::_from(HeapStorage::from(value))
-    }
-}
-
-impl<T> LocalHeapRB<T> {
-    /// Creates a new local heap-allocated buffer with given capacity and zeroed (uninitialised) elements.
-    /// # Safety
-    /// The buffer must be then initialised using proper [`ProdIter`] methods (`*_init` ones).
-    pub unsafe fn new_zeroed(capacity: usize) -> Self {
-        let mut v = Vec::with_capacity(capacity);
-
-        for _ in 0..capacity {
-            v.push(UnsafeSyncCell::new_zeroed());
-        }
-
-        Self::_from(HeapStorage::from(v))
-    }
-
-    /// Creates a new local heap-allocated buffer with given capacity and elements initialised to `default`.
-    pub fn default(capacity: usize) -> Self
-        where T: Default + Clone {
-        Self::from(vec![T::default(); capacity])
-    }
-}
+impl_rb!(LocalHeapRB);
